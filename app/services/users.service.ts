454e19e5f2e96a16923ebbd19c015b5da59b9174
Dima Panasyuk <dima_panasyuk@ukr.net>
2016-07-03 00:45:47 +0300

    changed mocked users

--- app/services/users.service.ts
+++ app/services/users.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 export class UsersService {
   users: IUser[] = [
     new User('Dima', 'Panasyuk', 'mitya', new Date('11/10/1997').getTime(), 18, 'http://www.suave.com/Images/2072/2072-665335-Category%20Hero%20Images_Mens.png', []),
-    new User('Nazar', 'Shimko', 'shimko', new Date('24/11/1996').getTime(), 19, 'http://67.media.tumblr.com/1233fb3e9e209c28e49da269b0de301a/tumblr_inline_n143bfJWJR1rzpf4l.png', []),
-    new User('Anna', 'Kushik', 'kushik', new Date('4/1/1996').getTime(), 19, 'http://kingofwallpapers.com/girl/girl-017.jpg', [])
+    new User('Addy', 'Osmani', 'osmani', new Date('24/11/1992').getTime(), 19, 'http://67.media.tumblr.com/1233fb3e9e209c28e49da269b0de301a/tumblr_inline_n143bfJWJR1rzpf4l.png', []),
+    new User('Deborah', 'Kurata', 'dkurata', new Date('4/1/1985').getTime(), 19, 'http://kingofwallpapers.com/girl/girl-017.jpg', [])
   ];
   getUsers(): IUser[] {
     return this.users;
