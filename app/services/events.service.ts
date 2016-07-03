f9b6730416d5e80e06c9f78872bf6a2960d360b4
Dima Panasyuk <dima_panasyuk@ukr.net>
2016-07-03 10:07:31 +0300

    added styles for tags

--- app/services/events.service.ts
+++ app/services/events.service.ts
@@ -5,19 +5,22 @@ import * as _ from 'lodash';
 
 @Injectable()
 export class EventsService {
+  
+  // Better to pass object
+  // Replace mocked data into api folder
   events: IEvent[] = [
-    new Event(1, 'Title 1', 'Description 1', new Date().getTime(), new Date().getTime(), 'Ternopil', '', [], this._usersService.getUsers()[0], 'http://ichef.bbci.co.uk/naturelibrary/images/ic/credit/640x395/m/mo/mountain/mountain_1.jpg', false, 1.4),
-    new Event(2, 'Title 2', 'Description 2', new Date().getTime(), new Date().getTime(), 'Ternopil', '', [], this._usersService.getUsers()[1], 'http://www.telegraph.co.uk/content/dam/Travel/galleries/travel/activityandadventure/The-worlds-most-beautiful-mountains/mountains-matterho_3374098a-large.jpg', true, 120),
-    new Event(3, 'Title 3', 'Description 3', new Date().getTime(), new Date().getTime(), 'Ternopil', '', [], this._usersService.getUsers()[2], 'http://vignette1.wikia.nocookie.net/prydain/images/7/7f/Mountains.jpg/revision/latest?cb=20130328022425', true, 500),
+    new Event(1, 'Title 1', 'Description 1', new Date().getTime(), new Date().getTime(), 'Ternopil', '', [], this._usersService.getUsers()[0], 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Transantarctic_mountain_hg.jpg', false, 1.4),
+    new Event(2, 'Title 2', 'Description 2', new Date().getTime(), new Date().getTime(), 'Ternopil', '', [], this._usersService.getUsers()[1], 'http://www.betterfood.co.uk/wp-content/uploads/2010/12/cafe-muffins.jpg', true, 120),
+    new Event(3, 'Title 3', 'Description 3', new Date().getTime(), new Date().getTime(), 'Ternopil', '', [], this._usersService.getUsers()[2], 'http://kingofwallpapers.com/mountain/mountain-024.jpg', true, 500),
   ];
   constructor(private _usersService: UsersService) {}
   getEvents(): IEvent[] {
     return this.events;
-  }
-
-  // Refactor 
+  } 
   createEvent(event:IEvent) {
-    this.events.push(new Event(event.id, event.title, event.description, event.creationDate, event.eventDate, event.address, event.geoPosition, event.tags, event.creator, event.picture, event.liked, event.distance));
+    this.events.push(new Event(event.id, event.title, event.description, 
+      event.creationDate, event.eventDate, event.address, event.geoPosition, 
+      event.tags, event.creator, event.picture, event.liked, event.distance));
   }
   like(id: number): void {
     let event = _.find(this.events, {id: id});
--- app/services/users.service.ts
+++ app/services/users.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UsersService {
+  
+  // Better to pass object
+  // Replace mocked data into api folder
   users: IUser[] = [
     new User('Dima', 'Panasyuk', 'mitya', new Date('11/10/1997').getTime(), 18, 'http://www.suave.com/Images/2072/2072-665335-Category%20Hero%20Images_Mens.png', []),
     new User('Addy', 'Osmani', 'osmani', new Date('24/11/1992').getTime(), 19, 'http://67.media.tumblr.com/1233fb3e9e209c28e49da269b0de301a/tumblr_inline_n143bfJWJR1rzpf4l.png', []),
